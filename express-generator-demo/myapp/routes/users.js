a89d902be94996f87496221e48ce45106c2be53f
acexu <578599619@qq.com>
2019-12-01 21:26:34 +0800

    增加文件上传下载功能

--- express-generator-demo/myapp/routes/users.js
+++ express-generator-demo/myapp/routes/users.js
@@ -1,39 +1,103 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const model = require('../model/index');
+const model = require("../model/index");
+var fs = require("fs");
+var multiparty = require("multiparty");
+var util = require("util");
 
 var Kitten = model.Kitten;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    // Kitten.remove({}, function(err, doc) {
-    //     if (err) {
-    //         console.log(err)
-    //     };
-    //     console.log(doc);
+router.get("/", function(req, res, next) {
+  // Kitten.remove({}, function(err, doc) {
+  //     if (err) {
+  //         console.log(err)
+  //     };
+  //     console.log(doc);
+  // });
+  Kitten.create({ time: new Date("1575081667860") }, function(err, docs) {
+    if (err) {
+      console.log(err);
+    }
+    console.log(docs);
+  });
+  Kitten.findOne({ time: { $exists: true } }, function(err, docs) {
+    if (err) {
+      console.log(err);
+    }
+    console.log(docs);
+    // var result = [];
+    // docs.forEach(function(item, index) {
+    //   result.push({ name: item.name, age: item.age || "" });
     // });
-    // Kitten.create({ 'age': 28 }, function(err, docs) {
-    //     if (err) {
-    //         console.log(err)
-    //     };
-    //     console.log(docs)
-    // });
-    Kitten.find({},
-        function(err, docs) {
-            if (err) {
-                console.log(err)
-            };
-            console.log(docs);
-            var result = [];
-            docs.forEach(function(item, index) {
-                result.push({ name: item.name, age: item.age || '' });
-            })
-            console.log(result);
-            res.send({
-                result: result
-            });
-        });
+    // console.log(result);
+    res.send({
+      result: docs
+    });
+  });
 });
 
+router.get("/downloadFile", function(req, res, err) {
+  //   fs.readFile("./public/徐国伟-职业生涯规划表-2019.xlsx", function(err, data) {
+  //     if (err) {
+  //       console.error(err);
+  //     }
+  res.download(
+    "./public/text/徐国伟-职业生涯规划表-2019.xlsx",
+    "职业生涯规划表.xlsx",
+    function(err) {
+      if (err) {
+        console.log(err);
+      }
+      console.log("文件下载成功");
+      res.end();
+    }
+  );
+  //   console.log(data);
+  // res.setHeader("Content-type", "text/html");
+  // res.send(data);
+  //   });
+});
+
+router.get("/getForm", function(req, res, err) {
+  if (err) {
+    console.error(err);
+  }
+  fs.readFile("./public/html/form.html", function(err, data) {
+    if (err) {
+      console.error(err);
+    }
+    res.write(data);
+    res.end();
+  });
+});
+
+router.post("/uploadFile", function(req, res, err) {
+  if (err) {
+    console.error(err);
+  }
+  var form = new multiparty.Form();
+  //设置编辑
+  form.encoding = "utf-8";
+  //设置文件存储路径
+  form.uploadDir = "./public/text/";
+  form.parse(req, function(err, fields, files) {
+    if (err) {
+      console.error(err);
+    }
+    console.log(files.upload[0].originalFilename);
+    console.log(files.upload[0].path);
+    //同步重命名文件名
+    fs.renameSync(
+      files.upload[0].path,
+      "./public/text/" + files.upload[0].originalFilename
+    );
+    console.log(files);
+    res.writeHead(200, { "content-type": "multipart/form-data" });
+    res.write("上传成功");
+    // res.end();
+    res.end(util.inspect({ fields: fields, files: files }));
+  });
+});
 
 module.exports = router;
--- express-generator-demo/myapp/schema/index.js
+++ express-generator-demo/myapp/schema/index.js
@@ -1,25 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 var kittySchema = mongoose.Schema({
-    name: {
-        type: String
-    },
-    age: {
-        type: Number
-    }
+  name: {
+    type: String
+  },
+  age: {
+    type: Number
+  },
+  time: {
+    type: Date
+  }
 });
 //向Schema中添加实例方法
 kittySchema.methods.speak = function() {
-    var greeting = this.name ?
-        "kitty name is " + this.name :
-        "I dont have a name";
-    console.log(greeting);
+  var greeting = this.name
+    ? "kitty name is " + this.name
+    : "I dont have a name";
+  console.log(greeting);
 };
 //向Schema中添加静态方法
 kittySchema.statics.speakLoudly = function() {
-    console.log('speak loudly');
+  console.log("speak loudly");
 };
 
 module.exports = {
-    kittySchema: kittySchema
-}
+  kittySchema: kittySchema
+};
